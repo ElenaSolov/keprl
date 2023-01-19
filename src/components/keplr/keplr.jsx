e65b70fa56083c569fea11da2cf58107efc93f7e
Elena Soloveva <seaver@yandex.ru>
2023-01-19 12:27:34 +0200

    Fix: clean up code

--- src/components/keplr/keplr.jsx
+++ src/components/keplr/keplr.jsx
@@ -7,39 +7,21 @@ const Keplr = () => {
 
    const [keplrUser, setKeplrUser] = useState("");
 
-// To create a readonly secret.js client, just pass in a gRPC-web endpoint
-const secretjs = new SecretNetworkClient({
-  grpcWebUrl,
-  chainId,
-});
+    // To create a readonly secret.js client, just pass in a gRPC-web endpoint
+    const secretjs = new SecretNetworkClient({
+      grpcWebUrl,
+      chainId,
+    });
 
-console.log(secretjs);
-
-const getBalanse = async () => {
-const {
-  balance: { amount },
-} = await secretjs.query.bank.balance(
-  {
-    address: "secret1ap26qrlp8mcq2pg6r47w43l0y8zkqm8a450s03",
-    denom: "uscrt",
-  } ,
-);
-return amount;
-}
-
-
-console.log(`I have ${getBalanse() / 1e6} SCRT!`);
-useEffect(()=> {
-window.keplr.getKey(chainId).then(response => {
-           setKeplrUser(response.name);
-           console.log(response)
-       })
-}, []);
-
-
+    useEffect(()=> {
+    window.keplr.getKey(chainId).then(response => {
+               setKeplrUser(response);
+               console.log(response)
+           });
+    }, []);
 
     return (
-        <div>Добрый день, {keplrUser}</div>
+        <div>Добрый день, {keplrUser.name}</div>
     );
 }
 
